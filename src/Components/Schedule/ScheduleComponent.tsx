80fef43bc20753f02a51e62b5d46219827c145f0
Cole-Harp <78562481+Cole-Harp@users.noreply.github.com>
2023-09-19 23:34:35 -0500

    Big commit, things are messy but we need examples

--- src/Components/Schedule/ScheduleComponent.tsx
+++ src/Components/Schedule/ScheduleComponent.tsx
@@ -1,14 +1,137 @@
-import React from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+"use client";
+import React, { useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import { EventInput, formatDate } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { Event } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+import { createEvent } from '@/lib/db_actions/createEvent';
 
-const CalendarPage: React.FC = () => {
+type DemoAppProps = {
+  events: Event[]
+};
+
+
+
+const Calendar: React.FC<DemoAppProps> = ({ events }) => {
+  const [weekendsVisible, setWeekendsVisible] = useState(true);
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+
+  const renderSidebar = () => {
+    return (
+      <div className='demo-app-sidebar'>
+        <div className='demo-app-sidebar-section'>
+          <h2>Instructions</h2>
+          <ul>
+            <li>Select dates and you will be prompted to create a new event</li>
+            <li>Drag, drop, and resize events</li>
+            <li>Click an event to delete it</li>
+          </ul>
+        </div>
+        <div className='demo-app-sidebar-section'>
+          <label>
+            <input
+              type='checkbox'
+              checked={weekendsVisible}
+              onChange={handleWeekendsToggle}
+            ></input>
+            toggle weekends
+          </label>
+        </div>
+        <div className='demo-app-sidebar-section'>
+          <h2>All Events ({currentEvents.length})</h2>
+          <ul>
+            {currentEvents.map(renderSidebarEvent)}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  const handleWeekendsToggle = () => {
+    setWeekendsVisible(!weekendsVisible);
+  };
+
+  const handleDateSelect = async (selectInfo: { view: { calendar: any; }; startStr: any; endStr: any; allDay: any; }) => {
+    let title = prompt('Please enter a new title for your event');
+    let content = prompt('Please enter a description for your event');
+    let date = new Date(selectInfo.startStr);
+    let start = new Date(selectInfo.startStr);
+    let end = new Date(selectInfo.endStr);
+    let role = "Player"; //Somehow add role selection
+    let calendarApi = selectInfo.view.calendar;
+    calendarApi.unselect();
+  
+    if (title) {
+      createEvent(title, content, date, start, end, role);
+  
+      calendarApi.addEvent({
+        id: "FILLER",
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
+
+  const handleEventClick = (clickInfo) => {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  };
+
+  const handleEvents = (events) => {
+    setCurrentEvents(events);
+  };
+
   return (
-    <div>
-      <h2>Calendar and Scheduler</h2>
-      <Calendar />
+    <div className='demo-app'>
+      {renderSidebar()}
+      <div className='demo-app-main'>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          initialView='dayGridMonth'
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          weekends={weekendsVisible}
+          initialEvents={events}
+          select={handleDateSelect}
+          eventContent={renderEventContent}
+          eventClick={handleEventClick}
+          eventsSet={handleEvents}
+        />
+      </div>
     </div>
   );
 };
 
-export default CalendarPage;
+function renderEventContent(eventInfo: { timeText: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; event: { title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; }; }) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  );
+}
+
+function renderSidebarEvent(event) {
+  return (
+    <li key={event.id}>
+      <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
+      <i>{event.title}</i>
+    </li>
+  );
+}
+
+export default Calendar;
--- src/app/(root)/(routes)/Schedule/page.tsx
+++ src/app/(root)/(routes)/Schedule/page.tsx
@@ -1,16 +1,47 @@
-import CalendarPage from "@/Components/Schedule/ScheduleComponent";
+import Calendar from "@/Components/Schedule/ScheduleComponent";
+import prisma_db from "../../../../../prisma/db";
+import { Event } from "@prisma/client";
+import { useUser } from '@clerk/nextjs';
 
-const Calendar = () => {
-    return (
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import { FormEvent } from "react";
+
+
+export const getServerSideProps=(async() => {
+  const id = useUser().user?.id
+
+  const user = await prisma_db.user.findUnique({
+    where: {
+      clerkUserId: id,
+    },
+  });
+
+  const events = await prisma_db.event.findMany({
+    where: {
+      role: user?.role,
+    },
+  });
+
+  return { props: { events } };
+}) satisfies GetServerSideProps<{
+  events: Event[]
+}>
+ 
+export default function Page({
+  events,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+
+
+
+  return (
+    <div>
       <div>
-        <div>
-        <CalendarPage />
+      <Calendar events={events}/>
+    </div>
+      <div>
+          <h1>Next Two Weeks</h1>
       </div>
-        <div>
-            <h1>Next Two Weeks</h1>
-        </div>
-      </div>
-    );
-  };
-  
-  export default Calendar;
+    </div>
+  );
+}
