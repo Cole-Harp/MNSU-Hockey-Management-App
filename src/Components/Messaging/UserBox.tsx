70f1add3ab2d51c59e19c37ccba935d81543692d
Skeeter Cathcart <skeetercathcart@gmail.com>
2023-09-27 19:03:07 -0500

    small changes to messaging dashboard layout. more to come when .env is reconciled

--- src/Components/Messaging/UserBox.tsx
+++ src/Components/Messaging/UserBox.tsx
@@ -1,11 +1,35 @@
 'use client'
 
-import { FC } from 'react'
+import { User } from '@prisma/client'
+import { FC, useCallback, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
+
+interface UserBoxProps{
+    data: User
+}
 
 interface UserBoxProps {}
 
-const UserBox = () => {
-  return <div>UserBox</div>
+const UserBox: React.FC<UserBoxProps> = ({data}) => {
+
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleClick = useCallback(() => {
+        setIsLoading(true);
+        axios.post('/api/conversations', { userId: data.id })
+        .then((data) => { 
+            router.push(`/conversations/${data.data.id}`);
+        })
+        .finally(() => setIsLoading(false));
+    }, [data, router]);
+
+
+  return (
+  <div>
+    UserBox
+  </div>)
 }
 
 export default UserBox
