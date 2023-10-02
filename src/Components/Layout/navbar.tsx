817566824018dca5c47a0abc688863836f430d7a
Skeeter Cathcart <skeetercathcart@gmail.com>
2023-10-02 18:03:24 -0500

    Changed landing page styling. Hopefully .env is removed as well

--- src/Components/Layout/navbar.tsx
+++ src/Components/Layout/navbar.tsx
@@ -12,11 +12,11 @@ import { MobileSidebar } from './mobile-sidebar'
 
 export const Navbar = () => {
     return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 h-16 bg-primary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 h-16 bg-mnsu_purple">
             <div className="flex items-center pl-1">
                 <MobileSidebar/>
                 <Link href="/">
-                    <h1 className={cn("pl-3 hidden md:block text-xl md:text-3xl font-bold text-white",
+                    <h1 className={cn("pl-3 hidden md:block text-xl md:text-3xl font-bold text-mnsu_gold",
                     font.className
                     )}>
                         MNSU Hockey
