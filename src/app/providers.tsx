2fcd0f459f20638a2a3c8058920faf63e25ce072
Cole-Harp <78562481+Cole-Harp@users.noreply.github.com>
2023-10-20 00:40:25 -0500

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,6 @@
 import { ReactNode } from "react";
 import { cn } from "../lib/utils";
 import { Inter } from 'next/font/google'
-import { UserProvider } from "@/lib/userContext";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,11 +11,9 @@ export default function Providers({ children }: { children: ReactNode }) {
 
   return (
 
-    <UserProvider>
       <body className={cn("bg-secondary", inter.className)}>
         {children}
       </body>
-    </UserProvider>
 
   );
 }
