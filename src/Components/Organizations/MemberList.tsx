3ec02993e7183d83906a3e600d3acbd7b9d9b308
Hiruy Gebregiorgis <hiruyekubay@gmail.com>
2023-11-01 11:24:04 -0500

    Update MemberList.tsx
    Added remove user functionality

--- src/Components/Organizations/MemberList.tsx
+++ src/Components/Organizations/MemberList.tsx
@@ -3,15 +3,18 @@
 import { useOrganization } from "@clerk/nextjs";
 import React from "react";
 import { Building } from 'lucide-react'
-
+import { useState } from "react";
 
 const MemberList  = () => {
-
+  const [disabled, setDisabled] = useState(false);
     const { membershipList, membership } = useOrganization({
         membershipList: {},
       });
 
- 
+    const remove = async () => {
+      setDisabled(true);
+      await membership.destroy();
+    };
     
   return (
     <div className = ' w-1/3 bg-red-500'>
@@ -25,9 +28,13 @@ const MemberList  = () => {
                 <div className = 'flex w-1/3 text-center items-center px-2'> 
                     {m.publicUserData.firstName} {m.publicUserData.lastName} :: {m.role}
                 </div>
-                <button className = 'border-2 border-slate-800 px-4 m-2  hover:bg-slate-200 rounded-full shadow-md shadow-slate-600 active:scale-95'>
-              Select
-            </button>
+                <button 
+                  className='border-2 border-slate-800 px-4 m-2 hover:bg-slate-200 rounded-full shadow-md shadow-slate-600 active:scale-95'
+                  onClick={remove}
+                  disabled={disabled}
+                >
+                    Remove {/* Updated button text */}
+                </button>
             </div>
             </li>
         ))}
