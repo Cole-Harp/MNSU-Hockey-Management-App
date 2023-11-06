756370bc9af04b6cf40149d4fdf43af70c29bf0a
Skeeter Cathcart <skeetercathcart@gmail.com>
2023-11-06 17:23:35 -0600

    idk

--- src/app/(root)/(routes)/Organization/(organization)/[organization]/page.tsx
+++ src/app/(root)/(routes)/Organization/(organization)/[organization]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import InvitationList from "@/Components/Organizations/InvitationList";
+import InviteUser from "@/Components/Organizations/InviteUser";
 import MemberList from "@/Components/Organizations/MemberList";
 import OrganizationList from "@/Components/Organizations/OrganizationList";
 import { ClerkLoaded, OrganizationSwitcher } from "@clerk/nextjs";
@@ -19,6 +20,7 @@ const OrganizationPage = async ({ params }: { params: IParams }) => {
       <div>
         <ClerkLoaded>
           <MemberList />
+          <InviteUser />
         </ClerkLoaded>
         
       </div>
